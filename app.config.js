bdded8cbae22041883bedb9bf832eb6a1a2363ee
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2026-05-28 20:41:31 +0800

    fix: align Expo SDK 55 app config

--- app.config.js
+++ app.config.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 
-const appJson = require('./app.json');
-
-module.exports = () => {
-  const config = appJson.expo;
+module.exports = ({ config }) => {
   const android = { ...config.android };
   const localGoogleServicesFile = './google-services.json';
   const googleServicesFile = process.env.GOOGLE_SERVICES_JSON
