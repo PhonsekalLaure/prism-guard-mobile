c4c09a21af442ad7ebc44c36315c468c91e6655e
Quervie Manrique <manriquequervie@gmail.com>
2026-05-30 10:03:21 +0800

    refactor: add map api key for the  android emulator

--- app.config.js
+++ app.config.js
@@ -2,9 +2,21 @@ const fs = require('fs');
 
 module.exports = ({ config }) => {
   const android = { ...config.android };
+  const mapsApiKey =
+    process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+    process.env.GOOGLE_MAPS_API_KEY;
   const localGoogleServicesFile = './google-services.json';
   const googleServicesFile = process.env.GOOGLE_SERVICES_JSON
     || (fs.existsSync(localGoogleServicesFile) ? localGoogleServicesFile : null);
+
+  android.config = { ...android.config };
+
+  if (mapsApiKey) {
+    android.config.googleMaps = {
+      ...(android.config.googleMaps || {}),
+      apiKey: mapsApiKey,
+    };
+  }
 
   if (googleServicesFile) {
     android.googleServicesFile = googleServicesFile;
